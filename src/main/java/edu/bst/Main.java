51a57842abc6809512320b49cd20fc54fc8a9ed0
Bushlya Fedor <fedor10401@mail.ru>
2025-03-01 19:52:14 +0300

    fix

--- src/main/java/edu/bst/Main.java
+++ src/main/java/edu/bst/Main.java
@@ -8,7 +8,6 @@ public class Main {
         tree.insert(7);
         tree.insert(6);
         tree.insert(4);
-//        tree.insert(1);
         tree.insert(2);
         tree.insert(8);
         System.out.println("Глубина: " + tree.depth());
