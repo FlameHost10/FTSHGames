5c72d95a276cab751d9f6841ea7957274773aec1
Bushlya Fedor <fedor10401@mail.ru>
2025-03-04 16:56:26 +0300

    first commit

--- src/main/java/edu/bst/Main.java
+++ src/main/java/edu/bst/Main.java
@@ -6,8 +6,8 @@ public class Main {
         tree.insert(5);
         tree.insert(3);
         tree.insert(7);
-        tree.insert(6);
-        tree.insert(4);
+//        tree.insert(6);
+//        tree.insert(4);
 //        tree.insert(2);
         tree.insert(8);
         System.out.println("Глубина: " + tree.depth());
