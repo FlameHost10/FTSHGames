9d83ec560acd24cdb282e8bafd9138e72bbaed31
Bushlya Fedor <fedor10401@mail.ru>
2025-03-06 14:25:00 +0300

    fix 10th line

--- src/main/java/edu/bst/Main.java
+++ src/main/java/edu/bst/Main.java
@@ -7,7 +7,7 @@ public class Main {
         tree.insert(3);
         tree.insert(7);
         tree.insert(6);
-        tree.insert(4);
+//        tree.insert(4);
         tree.insert(2);
         tree.insert(8);
         System.out.println("Глубина: " + tree.depth());
