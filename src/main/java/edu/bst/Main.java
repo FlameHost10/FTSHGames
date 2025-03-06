362e25edbb55ab183c86a8516ef94956db47d8ad
Bushlya Fedor <fedor10401@mail.ru>
2025-03-06 14:11:04 +0300

    fix 9th line

--- src/main/java/edu/bst/Main.java
+++ src/main/java/edu/bst/Main.java
@@ -6,7 +6,7 @@ public class Main {
         tree.insert(5);
         tree.insert(3);
         tree.insert(7);
-        tree.insert(6);
+//        tree.insert(6);
         tree.insert(4);
 //        tree.insert(2);
         tree.insert(8);
